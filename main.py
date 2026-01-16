5ddbd7ec895034a1ffaefa56572b7ee01ee2c371
E33aS42 <esommier@student.42.fr>
2026-01-16 14:17:22 +0100

    added html files with extra windows for testing with llama3.2 on koyeb

--- main.py
+++ main.py
@@ -18,10 +18,10 @@ load_dotenv() # load value from our environment variable file
 
 API_KEY_CREDITS = {os.getenv("API_KEY"): 5} # the value are credits that are subtracted from everytime someone uses the API_KEY (not implemented)
 print(API_KEY_CREDITS)
-# MODEL = 'llama3.2'
+MODEL = 'llama3.2'
 # MODEL = 'mistral'
 # MODEL = 'gemma3:1b'
-MODEL = 'tinyllama'
+# MODEL = 'tinyllama'
 MODEL = 'llama3.2:1b'
 
 app = FastAPI()
@@ -45,7 +45,7 @@ def verify_api_key(x_api_key: str = Header(None)):
 
 @app.get("/", response_class=HTMLResponse)
 async def get_index():
-    with open("index.html", "r") as f:
+    with open("results.html", "r") as f:
         return f.read()
 
 # @app.post("/analyze")
@@ -121,6 +121,7 @@ def parse_analysis(text):
 
     raw_parts = re.split(pattern, text, flags=re.MULTILINE)
     parsing_list  = [p.strip() for p in raw_parts if p and p.strip()]
+    # print(parsing_list)
     
     # re.split cuts the text into groups following the pattern and includes them in the list
     # strip() filter out empty strings and strip whitespace from each element using strip()
@@ -131,32 +132,6 @@ def parse_analysis(text):
     return parsing_dict
 
 
-# @app.post("/analyze")
-# async def analyze_file(file: UploadFile = File(...)):
-#     # Extract text from PDF
-#     content = await file.read()
-#     doc = fitz.open(stream=content, filetype="pdf")
-#     text = " ".join([page.get_text() for page in doc])
-    
-#     analysis_text = analyze_document(text)
-#     parsing_dict = {'Summary':'Summary', 
-#                     'Primary Focus':'Primary Focus', 
-#                     'Authors': 'Authors', 
-#                     'Main Ideas': 'Main Ideas', 
-#                     'Tables & Figures':'Tables & Figures'
-#                     }
-#     parsing_dict = parse_analysis(analysis_text)
-#     titles = ['Summary', 'Primary Focus', 'Authors', 'Main Ideas', 'Tables & Figures']
-    
-#     return {
-#             "analysis": analysis_text,
-#             "summary": parsing_dict[titles[0]],
-#             "primary_focus": parsing_dict[titles[1]],
-#             "authors": parsing_dict[titles[2]],
-#             "ideas": parsing_dict[titles[3]],
-#             "tables_figures": parsing_dict[titles[4]],
-#             }
-
 @app.post("/analyze")
 async def analyze_file(file: UploadFile = File(...)):
     # Extract text from PDF
@@ -165,6 +140,32 @@ async def analyze_file(file: UploadFile = File(...)):
     text = " ".join([page.get_text() for page in doc])
     
     analysis_text = analyze_document(text)
+    parsing_dict = {'Summary':'Summary', 
+                    'Primary Focus':'Primary Focus', 
+                    'Authors': 'Authors', 
+                    'Main Ideas': 'Main Ideas', 
+                    'Tables & Figures':'Tables & Figures'
+                    }
+    parsing_dict = parse_analysis(analysis_text)
+    titles = ['Summary', 'Primary Focus', 'Authors', 'Main Ideas', 'Tables & Figures']
+    
     return {
             "analysis": analysis_text,
+            "summary": parsing_dict[titles[0]],
+            "primary_focus": parsing_dict[titles[1]],
+            "authors": parsing_dict[titles[2]],
+            "ideas": parsing_dict[titles[3]],
+            "tables_figures": parsing_dict[titles[4]],
             }
+
+# @app.post("/analyze")
+# async def analyze_file(file: UploadFile = File(...)):
+#     # Extract text from PDF
+#     content = await file.read()
+#     doc = fitz.open(stream=content, filetype="pdf")
+#     text = " ".join([page.get_text() for page in doc])
+    
+#     analysis_text = analyze_document(text)
+#     return {
+#             "analysis": analysis_text,
+#             }
